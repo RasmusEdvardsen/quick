4f2f427eb1f1c7464ee27e7aeec6daa57f499b5d
Rasmus Edvardsen <edvardsen.rasmus@gmail.com>
2018-03-13 23:01:26 +0100

    leave event, reload client's room cards, new create room flow (fragment), multiple clients in room

--- app/src/main/java/com/example/edvardsen/quick/activities/CardMenuActivity.java
+++ app/src/main/java/com/example/edvardsen/quick/activities/CardMenuActivity.java
@@ -1,12 +1,17 @@
 package com.example.edvardsen.quick.activities;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.CardView;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.RelativeLayout;
 
 import com.example.edvardsen.quick.data.User;
 import com.example.edvardsen.quick.R;
+import com.example.edvardsen.quick.helpers.DefaultValues;
 import com.example.edvardsen.quick.web.IO;
 import io.socket.client.Socket;
 
@@ -14,10 +19,12 @@ public class CardMenuActivity extends AppCompatActivity {
 
     //TODO: FIX RECONNECTING ON LANDSCAPE/HORIZONTAL MODE CHANGE!
 
-    private Socket socket;
+    Socket socket;
     RelativeLayout relativeLayout;
-    private RelativeLayout cardMenuRelativeLayout;
-    CardView cardView;
+    RelativeLayout cardMenuRelativeLayout;
+
+    ImageView imageView;
+    EditText editText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +33,18 @@ public class CardMenuActivity extends AppCompatActivity {
 
         User.getInstance();
 
+        editText = findViewById(R.id.message_field_message);
+        imageView = findViewById(R.id.message_field_send_message);
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String[] roomNameAndClientToCouple = editText.getText().toString().split(" ");
+                //TODO: VALIDATE ON CLIENTCOUPLE! IF CLIENTCOUPLE IS NOT EMAIL VALIDATED, THEN TRY AGAIN!
+                socket.emit("privatecreate", roomNameAndClientToCouple[0], roomNameAndClientToCouple[1]);
+                Intent intent = new Intent(getBaseContext(), ChatActivity.class);
+                startActivity(intent);
+            }
+        });
         relativeLayout = findViewById(R.id.card_outer_relative_layout);
         cardMenuRelativeLayout = findViewById(R.id.card_menu_relative_layout);
         IO.configureActivity(this);
